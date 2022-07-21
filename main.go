72e698697e3fdf36978ca041a13aef4cf50c80a6
koki <cohky.github@gmail.com>
2022-07-21 22:18:11 +0900

    fix: リファクタ
    チャンネル名
    
    closes #41

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ type Env struct {
 	clientSecret           string
 	lineChannelAccessToken string
 	lineChannelSecret      string
+	channelName            string
 	url                    string
 	mongodbUri             string
 }
@@ -91,7 +92,8 @@ func getEnv() (env *Env, err error) {
 	env.clientSecret = os.Getenv("CLIENT_SECRET")
 	env.lineChannelAccessToken = os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
 	env.lineChannelSecret = os.Getenv("LINE_CHANNEL_SECRET")
-	env.url = "https://www.twitch.tv/kato_junichi0817"
+	env.channelName = "kato_junichi0817"
+	env.url = "https://www.twitch.tv/" + env.channelName
 	env.mongodbUri = os.Getenv("MONGODB_URI")
 
 	return
@@ -131,9 +133,8 @@ func getToken(env *Env) (token *Token, err error) {
 // チャンネル情報取得
 func getChannel(env *Env, token *Token) (*Channel, error) {
 	fmt.Println("チャンネル取得")
-	channelName := "kato_junichi0817"
 
-	req, _ := http.NewRequest(http.MethodGet, "https://api.twitch.tv/helix/search/channels?query="+channelName, nil)
+	req, _ := http.NewRequest(http.MethodGet, "https://api.twitch.tv/helix/search/channels?query="+env.channelName, nil)
 	req.Header.Set("Client-ID", env.clientId)
 	req.Header.Set("Authorization", "Bearer "+token.AccessToken)
 
@@ -157,7 +158,7 @@ func getChannel(env *Env, token *Token) (*Channel, error) {
 	_ = json.Unmarshal(b, data)
 
 	for _, c := range data.Channels {
-		if c.BroadcasterLogin == channelName {
+		if c.BroadcasterLogin == env.channelName {
 			return c, nil
 		}
 	}
